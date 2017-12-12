1e618fe7e9f050682af3e7061610ff718753f81e
kesh95 <kesharalahiru1@gmail.com>
2017-12-12 16:38:38 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 basic.forever(() => {
-    let p = hx711.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
+    let p = hx711.ping(DigitalPin.P1, DigitalPin.P2, SensorUnit.Inches);
     led.plotBarGraph(p, 0);
 })
