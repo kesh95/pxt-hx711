9415ae67c5f0e7cd131fcf1fbcb8f1a7ec9147ba
kesh95 <kesharalahiru1@gmail.com>
2017-12-12 16:27:47 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 basic.forever(() => {
-    let p = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
+    let p = hx711.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
     led.plotBarGraph(p, 0);
 })
