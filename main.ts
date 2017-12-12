d6a8bfd6fc212ad5aa52c072b23fb96b110e4d52
kesh95 <kesharalahiru1@gmail.com>
2017-12-12 16:37:27 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,23 +14,23 @@ enum SensorUnit {
 namespace hx711 {
     /**
      * Send a ping and get the echo time (in microseconds) as a result
-     * @param trig tigger pin
-     * @param echo echo pin
+     * @param SCK tigger pin
+     * @param DT echo pin
      * @param unit desired conversion unit
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
     //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
-    export function ping(SCK_pin: DigitalPin, DT_pin: DigitalPin, unit: SensorUnit, maxCmDistance = 500): number {
+    export function ping(SCK: DigitalPin, DT: DigitalPin, unit: SensorUnit, maxCmDistance = 500): number {
         // send pulse
-        pins.setPull(trig, PinPullMode.PullNone);
-        pins.digitalWritePin(trig, 0);
+        pins.setPull(SCK, PinPullMode.PullNone);
+        pins.digitalWritePin(SCK, 0);
         control.waitMicros(2);
-        pins.digitalWritePin(trig, 1);
+        pins.digitalWritePin(SCK, 1);
         control.waitMicros(10);
-        pins.digitalWritePin(trig, 0);
+        pins.digitalWritePin(SCK, 0);
 
         // read pulse
-        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+        const d = pins.pulseIn(DT, PulseValue.High, maxCmDistance * 58);
 
         switch (unit) {
             case SensorUnit.Centimeters: return d / 58;
